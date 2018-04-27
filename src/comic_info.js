ed5ec18ef4feb1b2dfe30f6832f12e04cd90ae62
ddbrennan <ddbren@gmail.com>
2018-04-27 16:38:31 -0400

    initial commit

---
+++ src/comic_info.js
@@ -0,0 +1,210 @@
+export default COMIC_INFO = [
+  {
+    title: "God, What An Asshole!",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/01-bible1.png",
+    book: "The Bible",
+    alt: "Let me just delete this ladder from my Sim's pool..."
+  },
+  {
+    title: "The Sodom Continues",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/02-bible2.png",
+    book: "The Bible",
+    alt: "Know thy neighbor"
+  },
+  {
+    title: "Don't Look Back In Anger",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/03-bible3.png",
+    book: "The Bible",
+    alt: "Sure, your wife's dead, but the walkway's never been less icy!"
+  },
+  {
+    title: "1001 Arabian Twilights",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/04-arabiannights1.png",
+    book: "Arabian Nights",
+    alt: "The sultan is gonna love 1001 Shades of Gray"
+  },
+  {
+    title: "More Like Sin-Bad At Sailing!",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/05-arabiannights2.png",
+    book: "Maybe it's best to stick to stand-up."
+  },
+  {
+    title: "Once More With Keeling",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/06-arabiannights3.png",
+    book: "There's always next story."
+  },
+  {
+    title: "On Stranger Tides",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/07-arabiannights4.png",
+    book: "Well, he tried."
+  },
+  {
+    title: "Riddle of the Sphinxster",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/09-oedipus1.png",
+    book: "Oedipus Rex",
+    alt: "The correct answer is 'a stick' for the riddle buffs out there."
+  },
+  {
+    title: "Sophomoric Humor",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/10-oedipus2.png",
+    book: "Oedipus Rex",
+    alt: "When you hold your eyes like that they look like balls too."
+  },
+  {
+    title: "I Can Tell That We Are Gawain to be Friends",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/11-kingarthur1.png",
+    book: "Le Morte d'Arthur",
+    alt: "You don't know how hard I hug!"
+  },
+  {
+    title: "Le Morte de Tact",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/12-kingarthur2.png",
+    book: "Le Morte d'Arthur",
+    alt: "He gives great helmet."
+  },
+  {
+    title: "Ye Olde Toppe Fourty",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/13-kingarthur3.png",
+    book: "Le Morte d'Arthur",
+    alt: "Back when music was actually GOOD."
+  },
+  {
+    title: "A Gripping Conclusion",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/14-kingarthur4.png",
+    book: "Le Morte d'Arthur",
+    alt: "In the novelization of this comic they kiss, for the 'shippers out there."
+  },
+  {
+    title: "The Winter of Incontinence",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/15-richardiii1.png",
+    book: "Richard III",
+    alt: "But uh, once I clean up can Swayze come back?"
+  },
+  {
+    title: "My Kingdom for a Really, Really Good Lawyer",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/16-richardiii2.png",
+    book: "Richard III",
+    alt: "At least he's got a strong future in the GOP."
+  },
+  {
+    title: "A Tale of Two Titties",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/17-ataleoftwocities1.png",
+    book: "A Tale of Two Cities",
+    alt: "They call the centerfold the 'cheek spread'"
+  },
+  {
+    title: "It is a Far, Far Dumber Thing I Do",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/18-ataleoftwocities2.png",
+    book: "A Tale of Two Cities",
+    alt: "I knew that tattoo of the Bastille on my chest would come in handy one day."
+  },
+  {
+    title: "The Best of Disguises, the Worst of Disguises",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/19-ataleoftwocities3.png",
+    book: "A Tale of Two Cities",
+    alt: "A risky move, seeing as we are in the epoch of incredulity."
+  },
+  {
+    title: "Gee, That's Swelling",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/20-thehardyboys1.png",
+    book: "The Hardy Boys",
+    alt: "I encompass, and I eclipse."
+  },
+  {
+    title: "A Tough One to Swallow",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/21-thehardyboys2.png",
+    book: "The Hardy Boys",
+    alt: "Sometimes our worst enemy is ourself."
+  },
+  {
+    title: "Getting to the Bottom of Things",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/22-thehardyboys3.png",
+    book: "The Hardy Boys",
+    alt: "Joe suddenly didn't want the Snack Pack on the side table."
+  },
+  {
+    title: "A Bindle of Trouble",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/23-thehardyboys4.png",
+    book: "The Hardy Boys",
+    alt: "I am become Chet, destroyer of worlds."
+  },
+  {
+    title: "Puttin' on the Ritz",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/24-thegreatgatsby1.png",
+    book: "The Great Gatsby",
+    alt: "Denim Tie Optional"
+  },
+  {
+    title: "The Green Light",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/25-thegreatgatsby2.png",
+    book: "The Great Gatsby",
+    alt: "So we beat off..."
+  },
+  {
+    title: "Moaning Myrtle",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/26-thegreatgatsby3.png",
+    book: "The Great Gatsby",
+    alt: "Maybe I need glasses."
+  },
+  {
+    title: "Remember Your Floaties",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/27-thegreatgatsby4.png",
+    book: "The Great Gatsby",
+    alt: "Coming this Summer: Weekend at Gatsby's"
+  },
+  {
+    title: "Riddles in the Dark",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/28-thehobbit1.png",
+    book: "The Hobbit",
+    alt: "They never see the ol' rip off your face coming!"
+  },
+  {
+    title: "Mother Died Today. And So Did He. And Her. Oh Jeez.",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/29-thestranger1.png",
+    book: "L'Etranger",
+    alt: "This is why I prefer to swim in pools."
+  },
+  {
+    title: "Camus-flage",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/30-thestranger2.png",
+    book: "L'Etranger",
+    alt: "The closer we are to our enemies, the further we are from harm."
+  },
+  {
+    title: "I'm Gonna Kick Your Azkaban",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/31-harrypotter1.png",
+    book: "Harry Potter",
+    alt: "Snitches get magical healing spells that leave no scarring."
+  },
+  {
+    title: "The Mirror of Erised Citore",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/32-harrypotter2.png",
+    book: "Harry Potter",
+    alt: "'It's wingardium levi-oh-oh-oh-oh-sah' Ron ejaculated loudly."
+  },
+  {
+    title: "Right in the Beans",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/33-endersgame1.png",
+    book: "Ender's Game",
+    alt: "Are you sure you want me to etch that?"
+  },
+  {
+    title: "Locke Slash Peter",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/34-endersgame2.png",
+    book: "Ender's Game",
+    alt: "Who cares about blowing up some bugs? We've got slash fiction now."
+  },
+  {
+    title: "I Brake for Turtles",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/35-grapesofwrath2.png",
+    book: "The Grapes of Wrath",
+    alt: "This is good. The plot was moving a little too quick for my taste."
+  },
+  {
+    title: "Land of Milk and Honey",
+    img: "https://s3.us-east-2.amazonaws.com/critterbury/comics/36-grapesofwrath3.png",
+    book: "The Grapes of Wrath",
+    alt: "Grapes? They're canteloupes at least."
+  }
+]
+
